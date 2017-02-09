2c2ed11b584a33f93ee47b6878e101828ccc324d
ccataldo345 <christian.cataldo@itcollege.ee>
2017-02-09 12:41:55 +0200

    Initial commit - first lecture files

---
+++ Bank.rb
@@ -0,0 +1,14 @@
+File.open('input.csv', 'r') do |the_file|
+
+	lines = the_file.readlines.map do |line|
+		line.chomp.split(',')
+	end
+
+	lines = lines[1..-1]
+	debit_total = ''
+	lines.each do |line|
+		debit_total = debit_total + line[2]
+	end
+
+	puts debit_total
+end
