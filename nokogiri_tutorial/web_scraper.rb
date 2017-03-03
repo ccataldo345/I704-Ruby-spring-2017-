0a9cbb71c73d1306c5e0e45c1fad4a6dd9784596
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-03-03 17:05:52 +0200

    Iprove web_scraper.rb > prints ranks, titles and links

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 require 'nokogiri'
-# require 'json'
+require 'json'
 require 'pry'
 require 'csv'
 
@@ -8,17 +8,35 @@ page = HTTParty.get('http://news.ycombinator.com')
 
 parse_page = Nokogiri::HTML(page)
 
-pets_array = []
+myranks = []
+mytitles = []
+mylinks = []
 
 #this is where we parse the data
-parse_page.css('.storylink').map do |a|
-	post_name = a.text
-	pets_array.push(post_name)
+parse_page.css('.rank').map do |i|
+  post_name = i.text
+  myranks.push(post_name)
+end
+
+parse_page.css('.storylink').map do |i|
+  post_name = i.text
+  mytitles.push(post_name)
+end
+
+parse_page.css('.storylink').map do |i|
+  post_name = i.attributes["href"]   #parse links only from '.storylink' class
+  mylinks.push(post_name)
+end
+
+#print result
+(0..9).each do |i|
+  puts "#{myranks[i]} #{mytitles[i]}"  #also: #puts "#{i+1}) #{mytitles[i]}"
+  puts "   --> #{mylinks[i]}"           #also: #puts "   --> #{mylinks[i]}"
 end
 
 #push array into a csv file
-CSV.open('pets.csv', 'w') do |csv|
-	csv << pets_array
-end
+#CSV.open('data.csv', 'w') do |csv|
+#  csv << mytitles
+#end
 
 # Pry.start(binding)
